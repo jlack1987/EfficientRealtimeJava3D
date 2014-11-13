3d909e5e346f00123fe193d35ee59c262516c5be
Jordan <jlack1987@gmail.com>
2014-11-12 19:55:04 -0600

    Added an add method to Vector3d as well as a constructor with arguments and tests for each method

--- JavaMatrixLibrary/src/Vector3d.java
+++ JavaMatrixLibrary/src/Vector3d.java
@@ -1,7 +1,6 @@
 
 public class Vector3d 
 {
-
 	double x;
 	double y;
 	double z;
@@ -11,5 +10,27 @@ public class Vector3d
 		this.x = 0;
 		this.y = 0;
 		this.z = 0;
+	}
+	
+	public Vector3d(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	/**
+	 * Add method in which the vector argument 
+	 * is added to this and the result is stored 
+	 * in this, i.e. this = this + vector. The 
+	 * Vector3d argument is not modified.
+	 * 
+	 * @param vector
+	 */
+	public void add(Vector3d vector)
+	{
+		this.x = this.x + vector.x;
+		this.y = this.y + vector.y;
+		this.z = this.z + vector.z;
 	}
 }
--- JavaMatrixLibrary/test/Vector3dTest.java
+++ JavaMatrixLibrary/test/Vector3dTest.java
@@ -1,11 +1,61 @@
-import org.junit.Test;
+import static org.junit.Assert.*;
 
+import java.util.Random;
+
+import org.junit.Test;
 
 public class Vector3dTest {
 
+	Random random = new Random();
+	int nTests = 10;
+	
+	@Test
+	public void testVector3dEmptyConstructor()
+	{
+		Vector3d vector = new Vector3d();
+		
+		assertEquals(vector.x, 0.0, 1e-12);
+		assertEquals(vector.y, 0.0, 1e-12);
+		assertEquals(vector.z, 0.0, 1e-12);
+	}
+	
 	@Test
 	public void testVector3dConstructor()
 	{
-		Vector3d vector = new Vector3d();
+		for(int i = 0; i<nTests; i++)
+		{
+			double x = random.nextDouble();
+			double y = random.nextDouble();
+			double z = random.nextDouble();
+			
+			Vector3d vector = new Vector3d(x,y,z);
+			
+			assertEquals(x, vector.x, 1e-10);
+			assertEquals(y, vector.y, 1e-10);
+			assertEquals(z, vector.z, 1e-10);
+		}
+	}
+	
+	@Test
+	public void testVectorAdd()
+	{
+		for(int i = 0; i<nTests; i++)
+		{
+			double x1 = random.nextDouble();
+			double x2 = random.nextDouble();
+			double y1 = random.nextDouble();
+			double y2 = random.nextDouble();
+			double z1 = random.nextDouble();
+			double z2 = random.nextDouble();
+			
+			Vector3d vector1 = new Vector3d(x1,y1,z1);
+			Vector3d vector2 = new Vector3d(x2,y2,z2);
+			
+			vector1.add(vector2);
+			
+			assertEquals(x1+x2,vector1.x,1e-12);
+			assertEquals(y1+y2,vector1.y,1e-12);
+			assertEquals(z1+z2,vector1.z,1e-12);
+		}
 	}
 }
