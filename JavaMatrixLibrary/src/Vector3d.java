9d7e4d1257af5b3f34c18a4a10876b4bd94c3e40
Erin Lack <Erin@erins-mbp.attlocal.net>
2014-11-16 10:23:15 -0600

    fixed edge case in cross product of Vector3d

--- JavaMatrixLibrary/src/Vector3d.java
+++ JavaMatrixLibrary/src/Vector3d.java
@@ -1,36 +1,34 @@
-
-public class Vector3d 
+public class Vector3d
 {
 	double x;
 	double y;
 	double z;
-	
+
 	public Vector3d()
 	{
 		this.x = 0;
 		this.y = 0;
 		this.z = 0;
 	}
-	
+
 	public Vector3d(double x, double y, double z)
 	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-	
+
 	public Vector3d(Vector3d vector)
 	{
 		this.x = vector.x;
 		this.y = vector.y;
 		this.z = vector.z;
 	}
-	
+
 	/**
-	 * Add method in which the vector argument 
-	 * is added to this and the result is stored 
-	 * in this, i.e. this = this + vector. The 
-	 * Vector3d argument is not modified.
+	 * Add method in which the vector argument is added to this and the result
+	 * is stored in this, i.e. this = this + vector. The Vector3d argument is
+	 * not modified.
 	 * 
 	 * @param vector
 	 */
@@ -40,12 +38,11 @@ public class Vector3d
 		this.y = this.y + vector.y;
 		this.z = this.z + vector.z;
 	}
-	
+
 	/**
-	 * Subtract method in which the vector argument 
-	 * is subtracted to this and the result is stored 
-	 * in this, i.e. this = this - vector. The 
-	 * Vector3d argument is not modified.
+	 * Subtract method in which the vector argument is subtracted to this and
+	 * the result is stored in this, i.e. this = this - vector. The Vector3d
+	 * argument is not modified.
 	 * 
 	 * @param vector
 	 */
@@ -55,27 +52,35 @@ public class Vector3d
 		this.y = this.y - vector.y;
 		this.z = this.z - vector.z;
 	}
-	
+
 	/**
-	 * Dot method in which the elements of this
-	 * are multiplied by the corresponding 
-	 * elements of the vector and summed up.
-	 * Vector3d argument is not modified.
+	 * Dot method in which the elements of this are multiplied by the
+	 * corresponding elements of the vector and summed up. Vector3d argument is
+	 * not modified.
 	 * 
 	 * @param vector
 	 * @return
 	 */
 	public double dot(Vector3d vector)
 	{
-		return this.x*vector.x+this.y*vector.y+this.z*vector.z;
+		return this.x * vector.x + this.y * vector.y + this.z * vector.z;
 	}
-	
-	
+
 	public void cross(Vector3d vector, Vector3d vectorToPack)
 	{
-		vectorToPack.x = this.y*vector.z-this.z*vector.y;
-		vectorToPack.y = this.z*vector.x-this.x*vector.z;
-		vectorToPack.z = this.x*vector.y-this.y*vector.x;
+		if (this == vector || this == vectorToPack)
+		{
+			double tempx = this.x;
+			double tempy = this.y;
+			double tempz = this.z;
+			vectorToPack.x = tempy * vector.z - tempz * vector.y;
+			vectorToPack.y = tempz * vector.x - tempx * vector.z;
+			vectorToPack.z = tempx * vector.y - tempy * vector.x;
+		} else
+		{
+			vectorToPack.x = this.y * vector.z - this.z * vector.y;
+			vectorToPack.y = this.z * vector.x - this.x * vector.z;
+			vectorToPack.z = this.x * vector.y - this.y * vector.x;
+		}
 	}
-	
 }
--- JavaMatrixLibrary/test/Vector3dTest.java
+++ JavaMatrixLibrary/test/Vector3dTest.java
@@ -4,42 +4,43 @@ import java.util.Random;
 
 import org.junit.Test;
 
-public class Vector3dTest {
+public class Vector3dTest
+{
 
 	Random random = new Random();
 	int nTests = 10;
-	
+
 	@Test
 	public void testVector3dEmptyConstructor()
 	{
 		Vector3d vector = new Vector3d();
-		
+
 		assertEquals(vector.x, 0.0, 1e-12);
 		assertEquals(vector.y, 0.0, 1e-12);
 		assertEquals(vector.z, 0.0, 1e-12);
 	}
-	
+
 	@Test
 	public void testVector3dConstructor()
 	{
-		for(int i = 0; i<nTests; i++)
+		for (int i = 0; i < nTests; i++)
 		{
 			double x = random.nextDouble();
 			double y = random.nextDouble();
 			double z = random.nextDouble();
-			
-			Vector3d vector = new Vector3d(x,y,z);
-			
+
+			Vector3d vector = new Vector3d(x, y, z);
+
 			assertEquals(x, vector.x, 1e-10);
 			assertEquals(y, vector.y, 1e-10);
 			assertEquals(z, vector.z, 1e-10);
 		}
 	}
-	
+
 	@Test
 	public void testVectorAdd()
 	{
-		for(int i = 0; i<nTests; i++)
+		for (int i = 0; i < nTests; i++)
 		{
 			double x1 = random.nextDouble();
 			double x2 = random.nextDouble();
@@ -47,22 +48,22 @@ public class Vector3dTest {
 			double y2 = random.nextDouble();
 			double z1 = random.nextDouble();
 			double z2 = random.nextDouble();
-			
-			Vector3d vector1 = new Vector3d(x1,y1,z1);
-			Vector3d vector2 = new Vector3d(x2,y2,z2);
-			
+
+			Vector3d vector1 = new Vector3d(x1, y1, z1);
+			Vector3d vector2 = new Vector3d(x2, y2, z2);
+
 			vector1.add(vector2);
-			
-			assertEquals(x1+x2,vector1.x,1e-12);
-			assertEquals(y1+y2,vector1.y,1e-12);
-			assertEquals(z1+z2,vector1.z,1e-12);
+
+			assertEquals(x1 + x2, vector1.x, 1e-12);
+			assertEquals(y1 + y2, vector1.y, 1e-12);
+			assertEquals(z1 + z2, vector1.z, 1e-12);
 		}
 	}
-	
+
 	@Test
 	public void testVectorSubtract()
 	{
-		for(int i = 0; i<nTests; i++)
+		for (int i = 0; i < nTests; i++)
 		{
 			double x1 = random.nextDouble();
 			double x2 = random.nextDouble();
@@ -70,22 +71,22 @@ public class Vector3dTest {
 			double y2 = random.nextDouble();
 			double z1 = random.nextDouble();
 			double z2 = random.nextDouble();
-			
-			Vector3d vector1 = new Vector3d(x1,y1,z1);
-			Vector3d vector2 = new Vector3d(x2,y2,z2);
-			
+
+			Vector3d vector1 = new Vector3d(x1, y1, z1);
+			Vector3d vector2 = new Vector3d(x2, y2, z2);
+
 			vector1.subtract(vector2);
-			
-			assertEquals(x1-x2,vector1.x,1e-12);
-			assertEquals(y1-y2,vector1.y,1e-12);
-			assertEquals(z1-z2,vector1.z,1e-12);
+
+			assertEquals(x1 - x2, vector1.x, 1e-12);
+			assertEquals(y1 - y2, vector1.y, 1e-12);
+			assertEquals(z1 - z2, vector1.z, 1e-12);
 		}
 	}
-	
+
 	@Test
 	public void testVectorDot()
 	{
-		for(int i=0; i<nTests; i++)
+		for (int i = 0; i < nTests; i++)
 		{
 			double x1 = random.nextDouble();
 			double x2 = random.nextDouble();
@@ -93,21 +94,21 @@ public class Vector3dTest {
 			double y2 = random.nextDouble();
 			double z1 = random.nextDouble();
 			double z2 = random.nextDouble();
-			
-			Vector3d vector1 = new Vector3d(x1,y1,z1);
-			Vector3d vector2 = new Vector3d(x2,y2,z2);
-			
+
+			Vector3d vector1 = new Vector3d(x1, y1, z1);
+			Vector3d vector2 = new Vector3d(x2, y2, z2);
+
 			double dotproduct = vector1.dot(vector2);
-			
-			assertEquals(x1*x2+y1*y2+z1*z2,dotproduct,1e-12);
-			
+
+			assertEquals(x1 * x2 + y1 * y2 + z1 * z2, dotproduct, 1e-12);
+
 		}
 	}
-	
+
 	@Test
 	public void testVectorCross()
 	{
-		for(int i=0; i<nTests; i++)
+		for (int i = 0; i < nTests; i++)
 		{
 			double x1 = random.nextDouble();
 			double x2 = random.nextDouble();
@@ -115,23 +116,23 @@ public class Vector3dTest {
 			double y2 = random.nextDouble();
 			double z1 = random.nextDouble();
 			double z2 = random.nextDouble();
-			
-			Vector3d vector1 = new Vector3d(x1,y1,z1);
-			Vector3d vector2 = new Vector3d(x2,y2,z2);
+
+			Vector3d vector1 = new Vector3d(x1, y1, z1);
+			Vector3d vector2 = new Vector3d(x2, y2, z2);
 			Vector3d vector3 = new Vector3d();
-			
-			vector1.cross(vector2,vector3);
-			
-			assertEquals(y1*z2-z1*y2,vector3.x,1e-12);
-			assertEquals(z1*x2-x1*z2,vector3.y,1e-12);
-			assertEquals(x1*y2-y1*x2,vector3.z,1e-12);
+
+			vector1.cross(vector2, vector3);
+
+			assertEquals(y1 * z2 - z1 * y2, vector3.x, 1e-12);
+			assertEquals(z1 * x2 - x1 * z2, vector3.y, 1e-12);
+			assertEquals(x1 * y2 - y1 * x2, vector3.z, 1e-12);
 		}
 	}
-	
+
 	@Test
 	public void testVectorCross2()
 	{
-		for(int i=0; i<nTests; i++)
+		for (int i = 0; i < nTests; i++)
 		{
 			double x1 = random.nextDouble();
 			double x2 = random.nextDouble();
@@ -139,15 +140,15 @@ public class Vector3dTest {
 			double y2 = random.nextDouble();
 			double z1 = random.nextDouble();
 			double z2 = random.nextDouble();
-			
-			Vector3d vector1 = new Vector3d(x1,y1,z1);
-			Vector3d vector2 = new Vector3d(x2,y2,z2);
-			
-			vector1.cross(vector2,vector1);
-			
-			assertEquals(y1*z2-z1*y2,vector1.x,1e-12);
-			assertEquals(z1*x2-x1*z2,vector1.y,1e-12);
-			assertEquals(x1*y2-y1*x2,vector1.z,1e-12);
+
+			Vector3d vector1 = new Vector3d(x1, y1, z1);
+			Vector3d vector2 = new Vector3d(x2, y2, z2);
+
+			vector1.cross(vector2, vector1);
+
+			assertEquals(y1 * z2 - z1 * y2, vector1.x, 1e-12);
+			assertEquals(z1 * x2 - x1 * z2, vector1.y, 1e-12);
+			assertEquals(x1 * y2 - y1 * x2, vector1.z, 1e-12);
 		}
 	}
 }
