ba00b2759bcd2f8dfd4e937ed6f1444aab67ffdf
Jordan <jlack1987@gmail.com>
2014-11-15 18:07:17 -0600

    Added .gitignore to ignore eclipse files. Added Matrix3d class and a class for unit testing it.

---
+++ JavaMatrixLibrary/src/Matrix3d.java
@@ -0,0 +1,27 @@
+
+public class Matrix3d 
+{
+	double m00, m01, m02;
+	double m10, m11, m12;
+	double m20, m21, m22;
+	
+	public Matrix3d()
+	{
+		m00 = m01 = m02 = 0;
+		m10 = m11 = m12 = 0;
+		m20 = m21 = m22 = 0;
+	}
+	
+	public Matrix3d(Matrix3d matrix)
+	{
+		this.m00 = matrix.m00;
+		this.m01 = matrix.m01;
+		this.m02 = matrix.m02;
+		this.m10 = matrix.m10;
+		this.m11 = matrix.m11;
+		this.m12 = matrix.m12;
+		this.m20 = matrix.m20;
+		this.m21 = matrix.m21;
+		this.m22 = matrix.m22;
+	}
+}
