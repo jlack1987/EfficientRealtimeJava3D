5c403c7ff3f1106a3a68f6f4899f3e263da5b278
Jordan <jlack1987@gmail.com>
2014-12-08 20:27:20 -0600

    Aded a few more tests for Matrix4d.

--- EfficientRealtimeJava3D/test/Matrix4dTest.java
+++ EfficientRealtimeJava3D/test/Matrix4dTest.java
@@ -105,6 +105,120 @@ public class Matrix4dTest
 			}
 		}
 	}
+	
+	@Test
+	public void testGetAndSetRow2()
+	{
+		Matrix4d matrix = new Matrix4d();
+		double[] vector = new double[4];
+		double[] vector1 = new double[4];
+		
+		for(int i = 0; i<nTests; i++)
+		{
+			for(int j = 0; j<4; j++)
+			{
+				randomizeDoubleArray(vector);
+				matrix.setRow(j, vector);
+				
+				matrix.getRow(j, vector1);
+				assertDoubleArrayEquals(vector,vector1,1e-16);
+			}
+		}
+	}
+	
+	@Test
+	public void testGetAndSetRow3()
+	{
+		Matrix4d matrix = new Matrix4d();
+		double[] vector = new double[4];
+		double[] vector1 = new double[4];
+		
+		for(int i = 0; i<nTests; i++)
+		{
+			for(int j = 0; j<4; j++)
+			{
+				double x = random.nextDouble();
+				double y = random.nextDouble();
+				double z = random.nextDouble();
+				double w = random.nextDouble();
+				vector[0] = x;
+				vector[1] = y;
+				vector[2] = z;
+				vector[3] = w;
+				matrix.setRow(j, x,y,z,w);
+				
+				matrix.getRow(j, vector1);
+				assertDoubleArrayEquals(vector,vector1,1e-16);
+			}
+		}
+	}
+	
+	@Test
+	public void testGetAndSetColumn()
+	{
+		Matrix4d matrix = new Matrix4d();
+		Vector4d vector = new Vector4d();
+		Vector4d vector1 = new Vector4d();
+		for(int i = 0; i<nTests; i++)
+		{
+			for(int j = 0; j<4; j++)
+			{
+				randomizeVector4d(vector);
+				matrix.setColumn(j, vector);
+				
+				matrix.getColumn(j, vector1);
+				assertVector4dEquals(vector,vector1,1e-16);
+			}
+		}
+	}
+	
+	@Test
+	public void testGetAndSetColumn2()
+	{
+		Matrix4d matrix = new Matrix4d();
+		double[] vector = new double[4];
+		double[] vector1 = new double[4];
+		
+		for(int i = 0; i<nTests; i++)
+		{
+			for(int j = 0; j<4; j++)
+			{
+				randomizeDoubleArray(vector);
+				matrix.setColumn(j, vector);
+				
+				matrix.getColumn(j, vector1);
+				assertDoubleArrayEquals(vector,vector1,1e-16);
+			}
+		}
+	}
+	
+	@Test
+	public void testGetAndSetColumn3()
+	{
+		Matrix4d matrix = new Matrix4d();
+		double[] vector = new double[4];
+		double[] vector1 = new double[4];
+		
+		for(int i = 0; i<nTests; i++)
+		{
+			for(int j = 0; j<4; j++)
+			{
+				double x = random.nextDouble();
+				double y = random.nextDouble();
+				double z = random.nextDouble();
+				double w = random.nextDouble();
+				vector[0] = x;
+				vector[1] = y;
+				vector[2] = z;
+				vector[3] = w;
+				matrix.setColumn(j, x,y,z,w);
+				
+				matrix.getColumn(j, vector1);
+				assertDoubleArrayEquals(vector,vector1,1e-16);
+			}
+		}
+	}
+	
 	@Test
 	public void testMatrixAdd1()
 	{
@@ -114,13 +228,78 @@ public class Matrix4dTest
 		for(int i = 0; i<nTests; i++)
 		{
 			createRandomMatrix4d(matrix1);
+			matrix2.set(matrix1);
+			double a = random.nextDouble()*100-50;
+			
+			matrix1.add(a);
+			for(int k = 0; k<4; k++)
+			{
+				for(int j = 0; j<4; j++)
+				{
+					assertEquals(matrix1.get(k,j),matrix2.get(k,j)+a,1e-16);
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void testMatrixAdd2()
+	{
+		Matrix4d matrix1 = new Matrix4d();
+		Matrix4d matrix2 = new Matrix4d();
+		Matrix4d matrix3 = new Matrix4d();
+		
+		for(int i = 0; i<nTests; i++)
+		{
+			createRandomMatrix4d(matrix1);
 			createRandomMatrix4d(matrix2);
+			matrix3.add(matrix1,matrix2);
+			
+			for(int k = 0; k<4; k++)
+			{
+				for(int j = 0; j<4; j++)
+				{
+					assertEquals(matrix3.get(k,j),matrix1.get(k,j)+matrix2.get(k,j),1e-16);
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void testMatrixAdd3()
+	{
+		Matrix4d matrix1 = new Matrix4d();
+		Matrix4d matrix2 = new Matrix4d();
+		Matrix4d matrix3 = new Matrix4d();
+		
+		for(int i = 0; i<nTests; i++)
+		{
+			createRandomMatrix4d(matrix1);
+			createRandomMatrix4d(matrix2);
+			matrix3.set(matrix1);
+			matrix1.add(matrix1,matrix2);
+			
+			for(int k = 0; k<4; k++)
+			{
+				for(int j = 0; j<4; j++)
+				{
+					assertEquals(matrix1.get(k,j),matrix3.get(k,j)+matrix2.get(k,j),1e-16);
+				}
+			}
 		}
 	}
 	
 	private void randomizeVector4d(Vector4d vector)
 	{
 		vector.set(random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble());
+	}
+	
+	private void randomizeDoubleArray(double[] array)
+	{
+		for(int i = 0; i< array.length; i++)
+		{
+			array[i] = random.nextDouble()*100-50;
+		}
 	}
 	
 	private void createRandomMatrix4d(Matrix4d matrix)
@@ -149,6 +328,19 @@ public class Matrix4dTest
 		assertEquals(v1.y,v2.y,epsilon);
 		assertEquals(v1.z,v2.z,epsilon);
 		assertEquals(v1.w,v2.w,epsilon);
+	}
+	
+	public void assertDoubleArrayEquals(double[] array1, double[] array2, double epsilon)
+	{
+		if(array1.length != array2.length)
+		{
+			throw new RuntimeException("Arrays are not the same length.");
+		}
+		
+		for(int i = 0; i<array1.length; i++)
+		{
+			assertEquals(array1[i],array2[i],epsilon);
+		}
 	}
 	
 	public void assertMatrix4dEquals(Matrix4d matrix1, Matrix4d matrix2, double epsilon)
